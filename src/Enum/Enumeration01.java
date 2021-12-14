b664e18db5b4d1f4df29a15b6b5d0842a0c51f7e
ContinueDogRabbit <mrganzhou@163.com>
2021-12-14 20:53:15 +0800

    枚举类型学习笔记

--- src/Enum/Enumeration01.java
+++ src/Enum/Enumeration01.java
@@ -1,4 +1,35 @@
 package Enum;
 
 public class Enumeration01 {
+    public static void main(String[] args){
+        System.out.println(Season.SPRING);
+        System.out.println(Season.SUMMER);
+    }
+}
+enum Season{
+    SPRING("春天","温和"),SUMMER("夏天","炎热"),
+    AUTUMN("秋天","凉爽"),WINTER("冬天","寒冷");
+    private final String name;
+    private final String Desc;
+
+    Season(String name, String desc) {
+        this.name = name;
+        Desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return Desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Season{" +
+                "name='" + name + '\'' +
+                ", Desc='" + Desc + '\'' +
+                '}';
+    }
 }
